a6c28a1696a7d0f76f15e13346c922ac64e214e6
nachilloXA <99141941+nachilloXA@users.noreply.github.com>
2025-10-26 23:56:35 -0600

    Update TheStackInterface.java

--- CP1/Pilas/TheStackInterface.java
+++ CP1/Pilas/TheStackInterface.java
@@ -1,3 +1,6 @@
+// Fuente de diseño base de pila con límite de capacidad: estructura Stack<T> de Java
+// ChatGPT (GPT-5 Thinking)
+
 public interface TheStackInterface<Type> {
 
     Boolean push(Type item);
